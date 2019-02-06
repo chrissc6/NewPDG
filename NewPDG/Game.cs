f052dfe2b29d7343ec3fdda38b9b97b53350b5f5
Chris Caudill <christopherscaudill@gmail.com>
2019-02-06 14:53:07 -0500

    init-c

---
+++ NewPDG/Game.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewPDG
+{
+    class Game
+    {
+        Random rnd = new Random();
+
+        int GamesPlayed = 0;
+        int GamesPlayed2 = 1;
+        int GamesLast = 0;
+        int Rolls2 = 0;
+
+        public static void ClearCurrentConsoleLine()
+        {
+            int currentLineCursor = Console.CursorTop;
+            Console.SetCursorPosition(0, Console.CursorTop);
+            for (int i = 0; i < Console.WindowWidth; i++)
+                Console.Write(" ");
+            Console.SetCursorPosition(0, currentLineCursor);
+        }
+
+        int RollDice()
+        {
+            return rnd.Next(6) + 1;
+        }
+
+        (int, int) RunPigDiceGame()
+        {
+            var Total = 0;
+            var Rolls = 0;
+            int Die;
+
+
+            //Console.WriteLine("Rolling...");
+            if (GamesPlayed < GamesPlayed2)
+            {
+
+                do
+                {
+
+                    Rolls++;
+                    Rolls2++;
+                    Die = RollDice();
+                    if (Die != 1)
+                    {
+                        Total = Total + Die;
+                    }
+                } while (Die != 1);
+
+
+                GamesPlayed++;
+                GamesPlayed2++;
+
+                ClearCurrentConsoleLine();
+                Console.Write($" Current Game: {GamesPlayed} |");
+            }
+            return (Total, Rolls);
+
+        }
+
+        public void Run()
+        {
+            var BestTotal = 0;
+            var BestRolls = 0;
+            var counter = 0;
+            var GamesToPlay = 10000000000;
+            while (counter++ < GamesToPlay)
+            {
+                var (Total, Rolls) = RunPigDiceGame();
+                if (Total > BestTotal)
+                {
+                    BestTotal = Total;
+                    BestRolls = Rolls;
+                    GamesLast = GamesPlayed - GamesLast;
+                    Console.WriteLine($"Score: {BestTotal} in {BestRolls} rolls."
+                        + $" | Games played since last win {GamesLast}, Total Rolls: {Rolls2}");
+                }
+
+            }
+
+        }
+    }
+}
--- NewPDG/Program.cs
+++ NewPDG/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace NewPDG
@@ -10,6 +11,17 @@ namespace NewPDG
     {
         static void Main(string[] args)
         {
+            string text = "Game Starting... 3. 2. 1.\n";
+            foreach (char c in text)
+            {
+                Console.Write(c);
+                Thread.Sleep(75);
+            }
+            //Console.WriteLine("Game Starting...");
+            var play = new Game();
+            play.Run();
+            Console.WriteLine("Press any key to quit ...");
+            Console.ReadKey();
         }
     }
 }
